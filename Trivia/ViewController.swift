b0cd33d983985b9cbb30ed1a9724bace83d07260
Vincent <vincent@kieberl.com>
2018-03-15 11:13:17 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Vincent on 15/03/2018.
+//  Copyright © 2018 Native App Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
